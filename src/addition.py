c40e7f52b4c844e9c2aa07afb1b5ff2354ef6936
Test Dev <test@example.com>
2026-06-13 08:11:06 +0530

    test(security): add dummy secret token and basic addition script

---
+++ src/addition.py
@@ -0,0 +1,6 @@
+a=input()
+b=input()
+
+c=int(a)+int(b)
+print(c)
+print("Addition completed successfully!")
---
+++ src/leaked_secrets.py
@@ -0,0 +1,3 @@
+
+# A file containing a secret token to verify security guard
+my_token = "sk-or-v1-abcdef1234567890abcdef1234567890"# gitaegis:ignore
